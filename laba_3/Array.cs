d886c0a1db5a86a44b5ff0f2d6b7540388910f88
Vitaliya <54934320+vitamelnytska@users.noreply.github.com>
2020-03-18 15:42:13 +0200

    laba_3

---
+++ laba_3/Array.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laba_3
+{
+    class Array
+    {
+        private int[] array;
+        private int _start;
+        public bool error;
+        private int _length;
+        
+        
+        public Array(int start, int length)
+        {
+            _start = start;
+            array = new int[length];
+            _length = length;
+        }
+        public int this[int index]
+        {
+            set
+            {
+                index -= _start;
+                if ((index >= 0) && (index < _length))
+                {
+                    array[index] = value;
+                    error = false;
+
+                }
+                else error = true;
+            }
+            get
+            {
+                index -= _start;
+                if ((index >= 0) && (index < _length))
+                {
+                    error = false;
+                    return array[index];
+                }
+                else
+                {
+                    error = true;
+                    return 0;
+                }
+            }
+            
+            
+        }
+        public int Length_array
+        {
+            get{
+                return _length;
+            }
+            
+        }
+        
+    }
+    
+}
---
+++ laba_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace laba_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array a = new Array(0,5); // 0 <> 9
+            
+            int l = a.Length_array;
+            a[0] = 3;
+            a[1] = 5;
+            a[2] = 4;
+            a[3] = 21;
+            a[4] = 54;
+            Console.WriteLine("\tMelnytska Vitaliya IS-92 ");
+            int b = a[6];
+            int j = a[3];
+            Console.WriteLine(b);
+            if (b == 0) Console.WriteLine("Error.\nindex is out of the bounds of the array.");
+            Console.WriteLine(" element №4: "+ j);
+            Console.WriteLine("length of array: " + l);
+        }
+    }
+}
